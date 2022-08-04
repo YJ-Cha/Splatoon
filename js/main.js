2388554718194ba39e8b6d8254c3df79023bf3a4
YJ-Cha <107171334+YJ-Cha@users.noreply.github.com>
2022-08-04 14:09:13 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//section5 뮤비 슬라이드
+// var swiper = new Swiper(".s5Slider", {
+// 	slidesPerView: 3,
+//     centeredSlides: true,
+// 	spaceBetween: 30,
+// 	slidesPerGroup: 1,
+// 	loop: true,
+// 	loopFillGroupWithBlank: true,
+// 	navigation: {
+// 		nextEl: ".swiper-button-next",
+// 		prevEl: ".swiper-button-prev",
+// 	},
+// });
+
+// wow
+new WOW().init();
